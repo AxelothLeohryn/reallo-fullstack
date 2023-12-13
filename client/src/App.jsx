f7beed84dbc88be00703f2d954a1468183b843f1
AxelothLeohryn <alejandro97ss@gmail.com>
2023-12-13 22:52:02 +0100

    login,register,logout functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,102 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:5000";
+
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import "./App.css";
+import Authenticate from "./components/Authenticate";
+// import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogin = async (email, password) => {
+    try {
+      const response = await axios.post("user/login", { email, password });
+      localStorage.setItem("token", response.data.token);
+      setIsLoggedIn(true);
+      console.log("Login successful!", response.data);
+      toast.success(response.data.msg);
+    } catch (error) {
+      // Error Popups FRONT here ...
+      console.error(
+        "Registration error:",
+        error.response ? error.response.data : error
+      );
+      toast.error(error.response.data.msg);
+    }
+  };
+
+  const handleRegister = async (username, email, password) => {
+    try {
+      const response = await axios.post("/user/signup", {
+        username,
+        email,
+        password,
+      });
+      console.log("Registration successful", response.data);
+      toast.success(response.data.msg);
+      // Additional logic after successful registration, like redirecting to login
+    } catch (error) {
+      console.error(
+        "Registration error:",
+        error.response ? error.response.data : error
+      );
+      toast.error(error.response.data.msg);
+      // Display or log more detailed error information
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      // Retrieves the token from localStorage.
+      const token = localStorage.getItem("token");
+      // Sends a POST request to the /logout endpoint with the JWT token in the Authorization header.
+      await axios.post(
+        "/user/logout",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      // On successful logout, it removes the token from localStorage and updates the state to reflect that the user is logged out.
+      localStorage.removeItem("token");
+      setIsLoggedIn(false);
+      toast.success("Logged out");
+    } catch (error) {
+      // Error Popups FRONT here ...
+      console.error("Logout error:", error);
+      toast.error(error.response.data.msg);
+    }
+  };
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      {isLoggedIn ? (
+        <>
+          <Header handleLogout={handleLogout} />
+          <Main />
+          <Footer />
+        </>
+      ) : (
+        <Authenticate
+          handleLogin={handleLogin}
+          handleRegister={handleRegister}
+        />
+      )}
+      <Toaster position="top-right" />
     </>
   );
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan")
+var cors = require("cors");
+const morgan = require("morgan");
 //cors
 const path = require("path");
 
@@ -10,18 +11,19 @@ require("./config/mongo_atlas.js"); //BBDD MongoDB conection
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('combined'))
+app.use(cors());
+app.use(morgan("combined"));
 
 //Routes
 const apiRoutes = require("./routes/api.routes.js");
 const userRoutes = require("./routes/user.routes.js");
 
 app.use("/api", apiRoutes);
-app.use("/user", userRoutes)
+app.use("/user", userRoutes);
 
 //Non existing routes
 //Capture All 404 errors
-const errors = require('./middlewares/errors');
+const errors = require("./middlewares/errors");
 app.use(errors.error404);
 
 app.listen(port, () => {
