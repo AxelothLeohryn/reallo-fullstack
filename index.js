bc986646161e67917f318f2f72b33c07b98db0db
Alejandro Sanchez <133040928+AxelothLeohryn@users.noreply.github.com>
2023-12-17 23:46:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use("/user", userRoutes);
 //* Serve static assets in production, must be at this location of this file
 if (process.env.NODE_ENV === 'production') {
   //*Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client/dist'));
   
   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'dist','index.html')));
 }
